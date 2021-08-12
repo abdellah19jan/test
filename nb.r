0e889a1e03e5f6d64472b3a17b07bb373b15066d
abdellah19jan <abdellah19jan>
2021-08-12 10:26:43 +0000

    Add markdown

--- nb.r
+++ nb.r
@@ -1,9 +1,24 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Main
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Function
+
+# COMMAND ----------
+
 g <- function(x, y) {
   
   x - y
   
 }
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Application
 
 # COMMAND ----------
 
